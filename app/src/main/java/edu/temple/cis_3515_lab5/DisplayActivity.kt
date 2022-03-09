46493f49298f847f8170a47c30747c9dda7bf3d4
ErikRodriguez96 <tuf93296@temple.edu>
2022-03-09 13:25:22 -0500

    Initial commit

---
+++ app/src/main/java/edu/temple/cis_3515_lab5/DisplayActivity.kt
@@ -0,0 +1,11 @@
+package edu.temple.cis_3515_lab5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DisplayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.display_activity_layout)
+    }
+}
