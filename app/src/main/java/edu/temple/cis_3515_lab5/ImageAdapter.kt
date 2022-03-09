c0f7b88f91a62d526c55f01244cbf9f1615e98dd
ErikRodriguez96 <tuf93296@temple.edu>
2022-03-09 13:54:29 -0500

    Added images

--- app/src/main/java/edu/temple/cis_3515_lab5/ImageAdapter.kt
+++ app/src/main/java/edu/temple/cis_3515_lab5/ImageAdapter.kt
@@ -1,4 +1,44 @@
 package edu.temple.cis_3515_lab5
 
-class ImageAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ImageAdapter(_images: Array<ImageObject>, _myRecyclerViewFunc: (ImageObject) -> Unit) : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+
+    val images = _images
+    val myRecyclerViewFunc = _myRecyclerViewFunc
+
+    inner class ImageViewHolder(_view: View) : RecyclerView.ViewHolder(_view) {
+
+        val previewImageView: ImageView = _view.findViewById<ImageView>(R.id.imageView)
+        val previewTextDescription: TextView = _view.findViewById<TextView>(R.id.imageTextView)
+
+        lateinit var imageObject: ImageObject
+
+        init {
+            _view.setOnClickListener { myRecyclerViewFunc(imageObject) }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val layout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.image_desc_layout, parent, false)
+
+        return ImageViewHolder(layout)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        holder.previewImageView.setImageResource(images[position].resourceId)
+        holder.previewTextDescription.text = images[position].description
+
+        holder.imageObject = images[position]
+    }
+
+    override fun getItemCount(): Int {
+        return images.size
+    }
 }
